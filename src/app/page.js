4ccc3ac17dcab6d2c7f8bfc275cb0c638e151ec5
mbetindia <mbetindia@gmail.com>
2025-05-24 17:30:02 +0530

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-red-500 text-3xl font-bold">Hello World in Red</h1>
+      <p>Welcome to MBET Learning Platform</p>
+    </div>
+  );
+}
